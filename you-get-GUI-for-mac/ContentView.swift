0ad72dbde7c624d36a6d70484b1e024ebc241744
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2023-05-02 16:13:28 +0800

    Initial Commit

---
+++ you-get-GUI-for-mac/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  you-get-GUI-for-mac
+//
+//  Created by LiYanan2004 on 2023/5/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ you-get-GUI-for-mac/you_get_GUI_for_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  you_get_GUI_for_macApp.swift
+//  you-get-GUI-for-mac
+//
+//  Created by LiYanan2004 on 2023/5/2.
+//
+
+import SwiftUI
+
+@main
+struct you_get_GUI_for_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
